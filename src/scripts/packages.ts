126628a1805577063cad2d18eed6e988a019d574
苏向夜 <fu050409@163.com>
2024-03-28 19:26:44 +0800

    perf(packages): add data interface

--- src/scripts/packages.ts
+++ src/scripts/packages.ts
@@ -1,3 +1,32 @@
 import packages from "^/packages.json";
 
-export default packages;
+interface PackagesInfo {
+  [packageName: string]: {
+    name: string;
+    latestVersion: string;
+    description: string;
+    authors: { name: string; email: string }[];
+    license: string;
+    topics: string[];
+    urls: {
+      homepage: string;
+      documentation: string;
+      repository: string;
+    };
+    readme_url?: string;
+    requirements: string[];
+    dependencies: string[];
+    requires_infini: string;
+    lastUpdate: string;
+    distributions: {
+      version: string;
+      download_url: string;
+      hash: string;
+      yanked: boolean;
+    }[];
+  };
+}
+
+const pkgs: PackagesInfo = packages.packages;
+
+export default pkgs;
